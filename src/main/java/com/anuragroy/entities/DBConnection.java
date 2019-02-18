19f95785ca4cdb5e6fecaf748fed1b3aef40d3aa
Anurag Roy <anuragroy17@gmail.com>
2019-02-18 17:36:22 +0530

    Initial Commit.

---
+++ src/main/java/com/anuragroy/entities/DBConnection.java
@@ -0,0 +1,18 @@
+package com.anuragroy.entities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+//THIS CLASS IS USED TO RETURN THE DRIVER CONNECTING THE MYSQL DATABASE
+
+public class DBConnection {
+    private static final String USERNAME = "DB_YOUR_USERNAME";
+    private static final String PASSWORD = "DB_YOUR_PASSWORD";
+    private static final String CONN = "jdbc:mysql://localhost/hospital";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+    }
+}
